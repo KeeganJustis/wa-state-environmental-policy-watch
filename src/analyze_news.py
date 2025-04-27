194194e81b42c09d476e1e204d9824f76503f138
Keegan Justis <keeganjustis@gmail.com>
2025-04-26 18:08:49 -0700

    feat: Added langchain for news summarization

---
+++ src/analyze_news.py
@@ -0,0 +1,48 @@
+import requests
+import os
+import logging
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.chat_models import init_chat_model
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+import dotenv
+import getpass
+
+dotenv.load_dotenv()
+
+os.environ["LANGSMITH_TRACING"] = "true"
+
+if not os.environ.get("ANTHROPIC_API_KEY"):
+    os.environ["ANTHROPIC_API_KEY"] = getpass.getpass("Enter API key for Anthropic: ")
+
+
+def get_news_content(url):
+    try:
+        loader = WebBaseLoader(url)
+        return list(loader.lazy_load())
+    except requests.exceptions.InvalidSchema as e:
+        logging.error(f"Error loading URL {url}: {e}")
+        return []
+
+
+def analyze_news_content(websites_content):
+    try:
+        llm = init_chat_model("claude-3-5-sonnet-latest", model_provider="anthropic")
+        prompt = ChatPromptTemplate.from_template("If blank put \"\", Otherwise write a concise summary of new laws in the legislature for the following:\\n\\n{context}")
+        documents  = get_news_content(websites_content)
+        chain = create_stuff_documents_chain(llm, prompt)
+        result = chain.invoke({"context": documents})
+        return result
+    except Exception as e:
+        logging.error(f"Error analyzing news content: {e}")
+        return ""
+    # return websites_content.metadata['source'] , result
+    
+def analyze_all_summaries(list_of_summaries):
+    summaries = [analyze_news_content(summary) for summary in list_of_summaries]
+    all_summaries = ",".join(summaries)
+    final_result = analyze_news_content(all_summaries)
+    return final_result
+    
+    
+        
--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import get_news
-
+import analyze_news
 
 
 def main():
-    news_sites = get_news.get_news_sites("plastics washington state legislature", "2025-01-01")
+    # find the start date as 3 months ago
+    news_sites = get_news.get_news_sites("plastics washington state legislature", "2025-04-21")
     get_news.save_news_sites(news_sites)
-    get_news.get_urls(news_sites)
+    news_urls = get_news.get_urls(news_sites)
+    final_summary = analyze_news.analyze_all_summaries(news_urls)
+    print(final_summary)
+    # analyze the news sites with lang
+    # send out  summary of the news sites and summaries by email
 if __name__ == "__main__":
     main()
+
